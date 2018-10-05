4f532b0930f9d073eb42a94139167907990a4291
Denis Prytov <89159544310@ya.ru>
2018-10-05 22:12:58 +0300

    .

--- 1.1 Pattern Count Problem/main.cpp
+++ 1.1 Pattern Count Problem/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 int main()
 {
-	string::size_type size = 0;
-	
-	return 0;	 
+    string::size_type count = 0;
+    size_t flag = 0;
+	string s1 = "", s2 = "";
+    cin >> s1 >> s2;
+    for (size_t j = 0; j < s2.size(); ++j)
+    {
+        for (size_t i = 0; ; ++i)
+        {
+            if (!s1[i])
+            {
+                count++;
+                break;
+            }
+            if (s2[i + j] != s1[i])
+            {
+                break;
+            }
+        }
+    }
+    cout << count;	
+	return 0;
 }
